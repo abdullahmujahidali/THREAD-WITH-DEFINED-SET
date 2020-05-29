402a826eecbb342deac867fe54b1c1219b4c5125
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-05-29 03:29:20 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS 5
+void *PrintHello(void *threadid)
+{
+ printf("\n %p: Hello World!\n", threadid);
+ pthread_exit(NULL);
+}
+int main( )
+{
+ pthread_t threads [NUM_THREADS];
+ int rc, t;
+ for(t=0; t < NUM_THREADS; t++)
+ {
+ printf ("Creating thread %d\n", t);
+ rc = pthread_create (&threads[t], NULL, PrintHello, (void *) t );
+ if (rc) {
+ printf("ERROR; return code from pthread_create() is %d\n", rc);
+ exit(-1);
+ }
+ }
+ pthread_exit(NULL);
+ }
